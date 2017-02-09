3e7f277a79f2b3b04025eff9cdbca76306e7b147
mvarxer11 <mvarxer@mvarxermatomacbook-pro.local>
2017-02-09 21:39:12 +0800

    Initial Commit

---
+++ TipCal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipCal
+//
+//  Created by mvarxer on 17/2/9.
+//  Copyright © 2017年 mvarxer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
