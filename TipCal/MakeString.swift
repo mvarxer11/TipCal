439def9f735de6685549d446a2296084dc12689a
mvarxer <mvarxer@163.com>
2017-02-13 00:48:09 +0800

    makes code simple

---
+++ TipCal/MakeString.swift
@@ -0,0 +1,41 @@
+//
+//  MakeString.swift
+//  TipCal
+//
+//  Created by mvarxer on 17/2/13.
+//  Copyright © 2017年 mvarxer. All rights reserved.
+//
+
+import Foundation
+
+
+
+//参数：percentage 百分数
+//根据给的百分数返回ServiceString
+func serviceStringMake(percentage:Int)->String {
+    if percentage == 0 {
+        return "0% Service Included"
+    }else {
+        switch percentage {
+        case 1...5:
+            return String(percentage) + "% Service Poor"
+        case 6...10:
+            return String(percentage) + "% Service Fair"
+        case 11...15:
+            return String(percentage) + "% Service Good!"
+        case 16...20:
+            return String(percentage) + "% Service Great!"
+        default:
+            return String(percentage) + "% Service Excellent!"
+        }
+    }
+}
+
+//根据给定人数返回peopleSplitString
+func peopleSplitStringMake(number:Int)->String {
+    if number == 1 {
+        return "No Split"
+    }else {
+        return String(number) + " People Split"
+    }
+}
--- TipCal/ViewController.swift
+++ TipCal/ViewController.swift
@@ -8,118 +8,70 @@
 
 import UIKit
 
-let personArray = [
-            "No split",
-            "2 People Split",
-            "3 People Split",
-            "4 People Split",
-            "5 People Split",
-            "6 People Split",
-            "7 People Split",
-            "8 People Split",
-            "9 People Split",
-            "10 People Split"
-]
-
-let starArray = [
-            "0% Service Included",
-            "5% Service Poor",
-            "10% Service Fair",
-            "15% Service Good!",
-            "20% Service Great!",
-            "25% Service Excellent!",
-            "30% Service Excellent!",
-            "35% Service Excellent!",
-            "40% Service Excellent!",
-            "45% Service Excellent!",
-            "50% Service Excellent!"
-]
-
 class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate {
-
-    @IBOutlet weak var peopleTitle: UILabel!
-    @IBOutlet weak var starTitle: UILabel!
-    @IBOutlet weak var person1: UIButton!
-    @IBOutlet weak var person2: UIButton!
-    @IBOutlet weak var person3: UIButton!
-    @IBOutlet weak var person4: UIButton!
-    @IBOutlet weak var person5: UIButton!
-    @IBOutlet weak var personMore: UIButton!
-    var personCount = 1 //默认2人数 以此来判断pickerView的初始位置
     
+    @IBOutlet weak var peopleTitle: UILabel!
+    @IBOutlet weak var people1: UIButton!
+    @IBOutlet weak var people2: UIButton!
+    @IBOutlet weak var people3: UIButton!
+    @IBOutlet weak var people4: UIButton!
+    @IBOutlet weak var people5: UIButton!
+    @IBOutlet weak var peopleMore: UIButton!
+    var peopleCount = 2 //默认2人数 以此来判断pickerView的初始位置
+    
+    @IBOutlet weak var starTitle: UILabel!
     @IBOutlet weak var star1: UIButton!
     @IBOutlet weak var star2: UIButton!
     @IBOutlet weak var star3: UIButton!
     @IBOutlet weak var star4: UIButton!
     @IBOutlet weak var star5: UIButton!
     @IBOutlet weak var starMore: UIButton!
-    var starCount = 3  //默认三颗星  以此来判断pickerView的初始位置
+    var starCount = 15  //15% 默认三颗星  以此来判断pickerView的初始位置
     
     var pickerSelect = 0 //0表示未选，1为people，2为star
     
     
-    @IBAction func person(_ sender: UIButton) {
-        switch sender.tag {
-        case 1:
-            person1.isSelected = true
-            person2.isSelected = false
-            person3.isSelected = false
-            person4.isSelected = false
-            person5.isSelected = false
-            personMore.isSelected = false
-            personCount = 0
-            peopleTitle.text = "No Split"
-        case 2:
-            person1.isSelected = true
-            person2.isSelected = true
-            person3.isSelected = false
-            person4.isSelected = false
-            person5.isSelected = false
-            personMore.isSelected = false
-            personCount = 1
-            peopleTitle.text = "2 People Split"
-        case 3:
-            person1.isSelected = true
-            person2.isSelected = true
-            person3.isSelected = true
-            person4.isSelected = false
-            person5.isSelected = false
-            personMore.isSelected = false
-            personCount = 2
-            peopleTitle.text = "3 People Split"
-        case 4:
-            person1.isSelected = true
-            person2.isSelected = true
-            person3.isSelected = true
-            person4.isSelected = true
-            person5.isSelected = false
-            personMore.isSelected = false
-            personCount = 3
-            peopleTitle.text = "4 People Split"
-        case 5:
-            person1.isSelected = true
-            person2.isSelected = true
-            person3.isSelected = true
-            person4.isSelected = true
-            person5.isSelected = true
-            personMore.isSelected = false
-            personCount = 4
-            peopleTitle.text = "5 People Split"
-        default:
-            break
-        }
+    @IBAction func TapPeople(_ sender: UIButton) {
+        peopleCount = sender.tag
+        peopleStateSet(count: peopleCount)
+        peopleTitle.text = peopleSplitStringMake(number: peopleCount)
     }
-
-    @IBAction func personMore(_ sender: UIButton) {
+    
+    
+    func peopleStateSet(count:Int) {
+        people1.isSelected = (people1.tag <= count)
+        people2.isSelected = (people2.tag <= count)
+        people3.isSelected = (people3.tag <= count)
+        people4.isSelected = (people4.tag <= count)
+        people5.isSelected = (people5.tag <= count)
+        peopleMore.isSelected = (peopleMore.tag <= count)
+    }
+    
+    
+    @IBAction func peopleMore(_ sender: UIButton) {
         pickerSelect = 1
         setAlertView()
     }
     
-    @IBAction func star1(_ sender: UIButton) {
-        starCount = sender.tag
-        updateStarIcons()
+    @IBAction func tapStar(_ sender: UIButton) {
+        
+        starCount = sender.tag * 5
+        starStateSet(count: sender.tag)
+        starTitle.text = serviceStringMake(percentage: starCount)
+        
     }
-
+    
+    func starStateSet(count:Int) {
+        star1.isSelected = (star1.tag <= count)
+        star2.isSelected = (star2.tag <= count)
+        star3.isSelected = (star3.tag <= count)
+        star4.isSelected = (star4.tag <= count)
+        star5.isSelected = (star5.tag <= count)
+        starMore.isSelected = (starMore.tag <= count)
+    }
+    
+    
+    
     @IBAction func starMore(_ sender: UIButton) {
         pickerSelect = 2
         setAlertView()
@@ -132,27 +84,26 @@ class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelega
         pickerView.delegate = self
         pickerView.dataSource = self
         if pickerSelect == 1 {
-            pickerView.selectRow(personCount, inComponent: 0, animated: false)
+            pickerView.selectRow(peopleCount-1, inComponent: 0, animated: false)
         }else {
             pickerView.selectRow(starCount, inComponent: 0, animated: false)
         }
         
-        
         alertController.view.addSubview(pickerView)
         
-        let cancelBtn = UIAlertAction(title: "cancel", style: .cancel, handler: nil)
-        alertController.addAction(cancelBtn)
+        let OKBtn = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alertController.addAction(OKBtn)
         self.present(alertController, animated: true, completion: nil)
     }
     
-
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     //组件数
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
@@ -161,9 +112,9 @@ class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelega
     //每组的行数
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         if pickerSelect == 1 {
-            return personArray.count
+            return 100  //people 1-100
         }else {
-            return starArray.count
+            return 51 //(ServiceString 从0%到50%包含0所以是51个)
         }
         
         
@@ -172,9 +123,9 @@ class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelega
     //每行的标题
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         if pickerSelect == 1 {
-            return personArray[row]
+            return peopleSplitStringMake(number: row + 1 ) //至少1人
         }else {
-            return starArray[row]
+            return serviceStringMake(percentage: row)
         }
     }
     
@@ -182,79 +133,20 @@ class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelega
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         
         if pickerSelect == 1 {
-            peopleTitle.text = personArray[row]
-            personCount = row
-            switch row {
-            case 0:
-                person1.isSelected = true
-                person2.isSelected = false
-                person3.isSelected = false
-                person4.isSelected = false
-                person5.isSelected = false
-                personMore.isSelected = false
-                personCount = 0
-            case 1:
-                person1.isSelected = true
-                person2.isSelected = true
-                person3.isSelected = false
-                person4.isSelected = false
-                person5.isSelected = false
-                personMore.isSelected = false
-            case 2:
-                person1.isSelected = true
-                person2.isSelected = true
-                person3.isSelected = true
-                person4.isSelected = false
-                person5.isSelected = false
-                personMore.isSelected = false
-            case 3:
-                person1.isSelected = true
-                person2.isSelected = true
-                person3.isSelected = true
-                person4.isSelected = true
-                person5.isSelected = false
-                personMore.isSelected = false
-            case 4:
-                person1.isSelected = true
-                person2.isSelected = true
-                person3.isSelected = true
-                person4.isSelected = true
-                person5.isSelected = true
-                personMore.isSelected = false
-            default:
-                person1.isSelected = true
-                person2.isSelected = true
-                person3.isSelected = true
-                person4.isSelected = true
-                person5.isSelected = true
-                personMore.isSelected = true
-            }
-            
+            peopleTitle.text = peopleSplitStringMake(number: row + 1)
+            peopleCount = row + 1
+            peopleStateSet(count: peopleCount)
             
         }else {
-            starTitle.text = starArray[row]
+            starTitle.text = serviceStringMake(percentage: row)
             starCount = row
-            updateStarIcons()
+            starStateSet(count: starCount/5)
+            
         }
-  
-    }
-    
-    //Update persons according to personCount
-    func updatePersonIcons() {
         
     }
     
-    //Update stars according to starCount
-    func updateStarIcons() {
-        star1.isSelected = (star1.tag <= starCount)
-        star2.isSelected = (star2.tag <= starCount)
-        star3.isSelected = (star3.tag <= starCount)
-        star4.isSelected = (star4.tag <= starCount)
-        star5.isSelected = (star5.tag <= starCount)
-        starMore.isSelected = (starMore.tag <= starCount)
-        
-        // let services = ["Include", "Poor", "Fair", "Good", "Great", "Excellent"]
-        //starTitle.text = @( Service   %s)   5% * currentTag,  services[tag]
-    }
+    
+    
 }
 
